6dc5617d3de4fc833683dbc25ab7d6fab9ea1f46
Mishin <mishin.ivt@gmail.com>
2023-01-09 16:38:54 +0300

    init

---
+++ app/src/main/java/com/tl/fin/ui/login/LoggedInUserView.kt
@@ -0,0 +1,9 @@
+package com.tl.fin.ui.login
+
+/**
+ * User details post authentication that is exposed to the UI
+ */
+data class LoggedInUserView(
+        val displayName: String
+        //... other data fields that may be accessible to the UI
+)
